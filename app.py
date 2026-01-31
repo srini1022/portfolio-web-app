410c2331a0b9bb1c0c5c87000ad0ce74169cd2f4
deepthiv16 <deepthivpolangi16@gmail.com>
2026-02-01 03:26:18 +0530

    app

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ app.secret_key = 'your_secret_key_here'
 # ------------------------
 
 
+app.secret_key = os.getenv('SECRET_KEY')
 
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-
-app.secret_key = os.getenv('SECRET_KEY')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
 
 
 mysql = MySQL(app)
